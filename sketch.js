ca4b8694cccd7351ab900a7483476c4e62bca838
nobkz <tone.nobukazu@gmail.com>
2023-01-06 17:50:53 +0900

    formatted

--- sketch.js
+++ sketch.js
@@ -1,82 +1,81 @@
-const PlayerBottomMargin = 10;
-const PlayerAccelation = 1;
-const PlayerMoveResist = 0.1;
+const PlayerBottomMargin = 10
+const PlayerAccelation = 1
+const PlayerMoveResist = 0.1
 
-const BulletSpeed = 10;
-const PlayerBuleltInterval = 100;
+const BulletSpeed = 10
+const PlayerBuleltInterval = 100
 
-let player;
+let GameMode = 'InGame'
+
+let player
 
 const Bullets = {
-  bullets : [],
-  add(charactor){
-    const bullet = new Bullet(charactor.pos);
-    this.bullets.push(bullet);
+  bullets: [],
+  add(charactor) {
+    const bullet = new Bullet(charactor.pos)
+    this.bullets.push(bullet)
   },
-  update(){
-    this.bullets.forEach(bullet=>bullet.update());
-    this.bullets = this.bullets.filter(bullet=>bullet.alive());
+  update() {
+    this.bullets.forEach((bullet) => bullet.update())
+    this.bullets = this.bullets.filter((bullet) => bullet.alive())
   },
-  draw(){
-    this.bullets.forEach(bullet=>bullet.draw());
-  }
+  draw() {
+    this.bullets.forEach((bullet) => bullet.draw())
+  },
 }
 
 const Enemies = {
-  enemies : [],
-  schedule : [],
-  add(enemy){
-    this.enemies.push(enemy);
+  enemies: [],
+  schedule: [],
+  add(enemy) {
+    this.enemies.push(enemy)
   },
-  setScheudle(fromMillis, enemy){
-    this.schedule.push(
-      [fromMillis, enemy]
-    );
+  setScheudle(fromMillis, enemy) {
+    this.schedule.push([fromMillis, enemy])
   },
-  update(){
-    const now = millis();
-    this.schedule.forEach(([fromMillis, enemy])=>{
-      if(fromMillis < now){
-        this.add(enemy);
+  update() {
+    const now = millis()
+    this.schedule.forEach(([fromMillis, enemy]) => {
+      if (fromMillis < now) {
+        this.add(enemy)
       }
-    });
-    this.schedule = this.schedule.filter(s=>s[0] > now);
+    })
+    this.schedule = this.schedule.filter((s) => s[0] > now)
 
-    this.enemies.forEach(enemy=>enemy.update());
-    this.enemies = this.enemies.filter(enemy=>enemy.alive());
+    this.enemies.forEach((enemy) => enemy.update())
+    this.enemies = this.enemies.filter((enemy) => enemy.alive())
   },
-  draw(){
-    this.enemies.forEach(enemy=>enemy.draw());
-  }
+  draw() {
+    this.enemies.forEach((enemy) => enemy.draw())
+  },
 }
 
 const Input = {
-  horizontal(){
-    if(keyIsPressed){
-      if(key == "a"  || keyCode == LEFT_ARROW){
-        return -1;
-      }else if(key == "d" || keyCode == RIGHT_ARROW ){
-        return 1;
+  horizontal() {
+    if (keyIsPressed) {
+      if (key == 'a' || keyCode == LEFT_ARROW) {
+        return -1
+      } else if (key == 'd' || keyCode == RIGHT_ARROW) {
+        return 1
       }
     }
-    return 0;
+    return 0
   },
 }
 
+class Player {
+  constructor() {
+    this.pos = createVector(width / 2, height)
+    this.velocity = createVector(0, 0)
+    this.acceleaction = createVector(0, 0)
+    this.size = 20
 
-class Player{
-  constructor(){
-    this.pos = createVector(width/2, height  - 100);
-    this.velocity = createVector(0,0);
-    this.acceleaction = createVector(0,0);
-    this.size = 20;
-
-    setInterval(()=>{
-      Bullets.add(this);
-    },PlayerBuleltInterval)
+    setInterval(() => {
+      Bullets.add(this)
+    }, PlayerBuleltInterval)
   }
 
-  update(){
+  update() {
     /*
     this.acceleaction.x = 
       Input.horizontal() * PlayerAccelation
@@ -90,129 +89,144 @@ class Player{
     }else if(this.pos.x > width){
       this.pos.x = this.pos.x - width;
     }*/
-    this.pos.x = mouseX;
+    this.pos.x = mouseX
   }
 
-  draw(){
-    fill(100);
-    stroke(255);
-    rectMode(CENTER);
-    square(this.pos.x, this.pos.y, this.size);
+  draw() {
+    fill(100)
+    stroke(255)
+    rectMode(CENTER)
+    square(this.pos.x, this.pos.y, this.size)
   }
 }
 
-class Bullet{
-  constructor(pos){
-    this.pos = pos.copy();
-    this.velocity = createVector(0, -BulletSpeed);
-    this.size = 3;
+class Bullet {
+  constructor(pos) {
+    this.pos = pos.copy()
+    this.velocity = createVector(0, -BulletSpeed)
+    this.size = 3
+    this.color = color(random(255), random(255), random(255))
   }
 
-  alive(){
-    if( abs(this.pos.x - width/2) > width/2 
-    || abs(this.pos.y - height/2) > height/2
-    ){
-      return false;
+  alive() {
+    if (
+      abs(this.pos.x - width / 2) > width / 2 ||
+      abs(this.pos.y - height / 2) > height / 2
+    ) {
+      return false
     }
-    if(this.checkHittingEnemy()){
-      return false;
+    if (this.checkHittingEnemy()) {
+      return false
     }
-    return true;
+    return true
   }
 
-  checkHittingEnemy(){
-    for(let enemy of Enemies.enemies){
-      const dx = abs(this.pos.x - enemy.pos.x);
-      const dy = abs(this.pos.y - enemy.pos.y);
+  checkHittingEnemy() {
+    for (let enemy of Enemies.enemies) {
+      const dx = abs(this.pos.x - enemy.pos.x)
+      const dy = abs(this.pos.y - enemy.pos.y)
       const l = (this.size + enemy.size) / 2
-      if(dx < l && dy < l){
-        enemy.hit = true;
-        return true;
+      if (dx < l && dy < l) {
+        enemy.hit(this)
+        return true
       }
     }
-    return false;
+    return false
   }
 
-  update(){
-    this.pos.add(this.velocity);
+  update() {
+    this.pos.add(this.velocity)
   }
 
-  draw(){
-    ellipseMode(CENTER);
-    fill(255);
-    stroke(255);
-    circle(this.pos.x, this.pos.y, this.size);
+  draw() {
+    ellipseMode(CENTER)
+    fill(this.color)
+    stroke(this.color)
+    circle(this.pos.x, this.pos.y, this.size)
   }
 }
 
-class Enemy{
-  constructor(){
-    this.pos = createVector(0, 0);
+class Enemy {
+  constructor() {
+    this.pos = createVector(0, 0)
   }
 
-  alive(){
-    if( abs(this.pos.x - width/2) > width/2 
-    || abs(this.pos.y - height/2) > height/2
-    ){
-      return false;
+  hit() {}
+
+  alive() {
+    if (
+      abs(this.pos.x - width / 2) > width / 2 ||
+      abs(this.pos.y - height / 2) > height / 2
+    ) {
+      return false
     }
-    return true;
+    return true
   }
-  update(){
-    this.velocity.add(this.acceleaction);
-    this.pos.add(this.velocity);
-  }
-  draw(){}
-}
-
-class DropEnemy extends Enemy{
-  constructor(x, speed){
-    super();
-    this.color = color(random(255), random(255), random(255));
-    this.pos = createVector(x, 0);
-    this.velocity = createVector(0, speed);
-    this.acceleaction = createVector(0,0);
-    this.size = random(10, width/10);
-  }
-
-  update(){
-    super.update();
-    if( this.hit ){
-      this.velocity = createVector(0,0);
+  update() {
+    this.velocity.add(this.acceleaction)
+    this.pos.add(this.velocity)
+    if (this.pos.y > height) {
+      GameMode = 'GameOver'
     }
   }
-
-  draw(){
-    fill(this.color);
-    stroke(255);
-    rectMode(CENTER);
-    square(this.pos.x, this.pos.y, this.size);
-  }
+  draw() {}
 }
 
+class DropEnemy extends Enemy {
+  constructor(x, speed) {
+    super()
+    this.color = color(255)
+    this.pos = createVector(x, 0)
+    this.velocity = createVector(0, speed)
+    this.acceleaction = createVector(0, 0)
+    this.size = random(10, width / 10)
+    this.hitted = false
+  }
 
+  hit(bullet) {
+    if (!this.hitted) {
+      this.color = bullet.color
+      this.hitted = true
+    }
+  }
+
+  update() {
+    super.update()
+    if (this.hitted) {
+      this.velocity = createVector(0, 0)
+    }
+  }
+
+  draw() {
+    fill(this.color)
+    stroke(this.color)
+    rectMode(CENTER)
+    square(this.pos.x, this.pos.y, this.size)
+  }
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  player = new Player();
+  createCanvas(windowWidth, windowHeight)
+  blendMode(SCREEN)
+  player = new Player()
 }
 
 function draw() {
-  if( frameCount % 50 == 0){
-    Enemies.setScheudle(
-      1000 + millis(), new DropEnemy(random(width), 1)
-    );
+  if (GameMode === 'InGame') {
+    if (frameCount % 50 == 0) {
+      Enemies.setScheudle(1000 + millis(), new DropEnemy(random(width), 1))
+    }
+
+    clear()
+    background(50)
+
+    player.update()
+    Bullets.update()
+    Enemies.update()
+
+    player.draw()
+    Bullets.draw()
+    Enemies.draw()
+  } else if (GameMode === 'GameOver') {
   }
-
-
-  background(50);
-
-  player.update();
-  Bullets.update();
-  Enemies.update();
-
-
-  player.draw();
-  Bullets.draw();
-  Enemies.draw();
 }
